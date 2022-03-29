6b2842cc540a21b2d6479137f93f2f215569faa3
Mike Schinkel <mike@newclarity.net>
2022-03-29 05:44:30 -0400

    Simplify QuickTimeActivate.sh

--- QuickTimeActivate.sh
+++ QuickTimeActivate.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-osascript -l JavaScript <<'END'
+osascript -l JavaScript <<'EOD'
   eval(Library("Utils").load("Functions.js"))
-
-	const path = "/System/Applications/QuickTime Player.app"
-	const app = Application(path)
-  app.includeStandardAdditions = true
-	if ( ! app.frontmost ) {
-		app.activate()
-	}
-END
+  quicktime(ACTIVATE)
+EOD
